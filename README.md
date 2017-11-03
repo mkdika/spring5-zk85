926bff3c8a0d62df08206a9c3bc350c896b05813
Maikel Chandika <mkdika@gmail.com>
2017-11-03 10:05:33 +0700

    first init

---
+++ README.md
@@ -0,0 +1,7 @@
+## ZK 8.5 MVVM CRUD Project Example
+
+This is ZK 8.5, Spring 5, Spring Boot 2, CRUD Project Example.
+
+
+### References
+coming soon..
---
+++ src/main/java/com/mkdika/spring5zk85/Spring5zk85Application.java
@@ -0,0 +1,12 @@
+package com.mkdika.spring5zk85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5zk85Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5zk85Application.class, args);
+	}
+}
